969c9d4feda485d77ef3bd2dc8b1a43815baf68f
谁书-ss <yushuishu@qq.com>
2022-12-24 16:47:09 +0800

    feat:初始化工程

---
+++ src/main/java/com/shuishu/blog/ShuishuBlogBackendApplication.java
@@ -0,0 +1,22 @@
+package com.shuishu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author ：谁书-ss
+ * @date   ： 2022-12-24 16:46
+ * @IDE    ：IntelliJ IDEA
+ * @Motto  ：ABC(Always Be Coding)
+ * <p></p>
+ * @Description ：blog
+ */
+@SpringBootApplication
+public class ShuishuBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuishuBlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuishu/blog/ShuishuBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuishu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuishuBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
